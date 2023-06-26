361d0ef7473c8479d7c87fe8b0c5af8f520509ec
Sugandha <sugandhatiwary7677@gmail.com>
2023-06-26 23:00:33 +0530

    26june

--- 160.CycleDetectionUndirected.java
+++ 160.CycleDetectionUndirected.java
@@ -51,3 +51,69 @@ public class 160.CycleDetectionUndirected {
         
     }
 }
+
+
+// BFS --> 
+public class Solution{
+
+     public static String cycleDetection(int[][] edges, int n, int m) {
+        // Write your code here.
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for(int i = 0; i <=n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < m; i++){
+            int src = edges[i][0];
+            int dest = edges[i][1];
+
+            graph.get(src).add(dest);
+            graph.get(dest).add(src);
+        }
+        boolean [] visited = new boolean [n + 1];
+
+        for(int i =0; i <= n; i++){
+            if(visited[i] == false){
+                if(bfs(graph, i, visited)) return "Yes";
+            }
+        }
+
+        return "No";
+    }
+
+    static boolean bfs(ArrayList<ArrayList<Integer>> graph, int src, boolean []visited){
+
+        Queue<pair> q = new LinkedList<>();
+        q.add(new pair(src, -1));
+        visited[src] = true;
+
+        while(!q.isEmpty()){
+            pair p = q.poll();
+
+            for(int nbr : graph.get(p.node)){
+                if(visited[nbr] == false ){
+                    q.add(new pair(nbr, p.node));
+                    visited[nbr] = true;
+                }
+                else if(nbr != p.parent) return true;
+            }
+            
+        }
+
+        return false;
+       
+    }
+    
+    
+}
+
+class pair{
+    int node;
+    int parent;
+
+    pair(int n, int p){
+        node = n;
+        parent = p;
+    }
+}
